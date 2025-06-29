d325d43c9d667823e4dc4d9c39db7f49775f1a0b
YOUNES54CDFVD <younesboooyah@gmail.com>
2025-06-29 21:45:20 +0100

    Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,3 +1,5 @@
+
+import FadeInOnScroll from "@/components/FadeInOnScroll";
 import { Link } from "react-router-dom";
 import Navigation from "../components/Navigation";
 import Footer from "../components/Footer";
