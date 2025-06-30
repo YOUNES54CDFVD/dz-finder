33328d3d5bd853f97c5c4949998ad194eb70b742
YOUNES54CDFVD <younesboooyah@gmail.com>
2025-06-30 15:29:56 +0100

    Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { supabase } from "@/supabaseClient";
+import { supabase } from "/supabaseClient";
 import Navigation from "../components/Navigation";
 import Footer from "../components/Footer";
 import FadeInOnScroll from "@/components/FadeInOnScroll";
